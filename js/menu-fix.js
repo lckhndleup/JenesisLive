fd3c3239afd1df8cc8e403b780594b1d24c527b4
lckhndleup <maliyildiz0307@gmail.com>
2025-09-27 22:22:16 +0300

    menu fix

--- js/menu-fix.js
+++ js/menu-fix.js
@@ -21,42 +21,101 @@ Menu Hover Fix JavaScript - Dropdown sadece text üzerinde açılmalı
         var $link = $menuItem.children("a");
         var $submenu = $menuItem.children(".sub-menu, ul");
         var timeout;
+        var isAnimating = false; // Animasyon kontrolü
 
         // Sadece link üzerine hover olduğunda dropdown aç
         $link.on("mouseenter", function (e) {
           clearTimeout(timeout);
-          $submenu.stop(true, true).addClass("flexnav-show");
+
+          // Eğer dropdown zaten açık değilse ve animasyon yapılmıyorsa aç
+          if (!$submenu.hasClass("flexnav-show") && !isAnimating) {
+            isAnimating = true;
+            $submenu.stop(true, true).addClass("flexnav-show");
+
+            // Animasyon bittikten sonra flag'i sıfırla
+            setTimeout(function () {
+              isAnimating = false;
+            }, 300);
+          }
         });
 
         // Link'ten çıkıldığında gecikmeli kapatma
         $link.on("mouseleave", function (e) {
+          var relatedTarget = e.relatedTarget || e.toElement;
+
+          // Eğer mouse dropdown'a gidiyorsa kapatma
+          if (relatedTarget && $(relatedTarget).closest($submenu).length > 0) {
+            return;
+          }
+
           timeout = setTimeout(function () {
             // Eğer mouse dropdown'da değilse kapat
-            if (!$submenu.is(":hover")) {
+            if (!$submenu.is(":hover") && !isAnimating) {
               $submenu.removeClass("flexnav-show");
             }
-          }, 100);
+          }, 200); // Daha uzun süre ver
         });
 
         // Dropdown üzerinde hover olduğunda açık tut
         $submenu.on("mouseenter", function () {
           clearTimeout(timeout);
-          $(this).addClass("flexnav-show");
+          // Dropdown zaten açık olduğundan emin ol
+          if (!$(this).hasClass("flexnav-show")) {
+            $(this).addClass("flexnav-show");
+          }
         });
 
         // Dropdown'tan çıkıldığında kapat
-        $submenu.on("mouseleave", function () {
-          $(this).removeClass("flexnav-show");
-        });
+        $submenu.on("mouseleave", function (e) {
+          var relatedTarget = e.relatedTarget || e.toElement;
 
-        // Li elementinin boş alanına hover olduğunda hiçbir şey yapma
-        $menuItem.on("mouseenter", function (e) {
-          // Sadece li'nin kendisine hover olduğunda (link'e değil)
-          if (e.target === this) {
-            // Dropdown'ı kapatma mantığı
-            if (!$link.is(":hover") && !$submenu.is(":hover")) {
+          // Eğer mouse ana link'e dönüyorsa kapatma
+          if (relatedTarget && $(relatedTarget).closest($link).length > 0) {
+            return;
+          }
+
+          // Kısa gecikme ile kapat
+          setTimeout(function () {
+            if (!$link.is(":hover") && !isAnimating) {
               $submenu.removeClass("flexnav-show");
             }
+          }, 100);
+        });
+
+        // Li elementinin boş alanlarında dropdown davranışını kontrol et
+        $menuItem.on("mouseenter", function (e) {
+          // Eğer event li'nin kendisinden geliyorsa (link veya submenu'den değil)
+          if (e.target === this) {
+            // Dropdown açıksa ve mouse ne link'te ne de dropdown'ta değilse
+            if (
+              $submenu.hasClass("flexnav-show") &&
+              !$link.is(":hover") &&
+              !$submenu.is(":hover")
+            ) {
+              // Dropdown'ı kapat ama gecikmeli
+              timeout = setTimeout(function () {
+                if (!$link.is(":hover") && !$submenu.is(":hover")) {
+                  $submenu.removeClass("flexnav-show");
+                }
+              }, 150);
+            }
+          }
+        });
+
+        // Li elementinden çıkıldığında da kontrol et
+        $menuItem.on("mouseleave", function (e) {
+          var relatedTarget = e.relatedTarget || e.toElement;
+
+          // Eğer mouse tamamen menü alanından çıkıyorsa dropdown'ı kapat
+          if (
+            !relatedTarget ||
+            (!$(relatedTarget).closest($menuItem).length &&
+              !$(relatedTarget).closest($submenu).length)
+          ) {
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+              $submenu.removeClass("flexnav-show");
+            }, 100);
           }
         });
       });
